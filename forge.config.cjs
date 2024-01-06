66f5425a9ab76ba94a764df0f555916bae8694c5
Satnam Sandhu <satnam@Satnams-MacBook-Pro.local>
2024-01-06 17:14:23 +0530

    added base react & electron config

---
+++ forge.config.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+  packagerConfig: {
+    asar: true,
+    icon: "./icons/icon",
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        setupIcon: "./icons/icon.ico",
+      },
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {
+        options: {
+          icon: "./icons/icon.png",
+        },
+      },
+    },
+    // {
+    //   name: "@electron-forge/maker-dmg",
+    //   config: {
+    //     icon: "./icons/icon.icns",
+    //   },
+    // },
+  ],
+  plugins: [
+    {
+      name: "@electron-forge/plugin-auto-unpack-natives",
+      config: {},
+    },
+  ],
+};
---
+++ main.js
@@ -0,0 +1,40 @@
+"use strict";
+
+import path from "path";
+import url from "url";
+import { app, BrowserWindow } from "electron";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: path.join(__dirname, "./icons/icon.png"),
+  });
+
+  const startUrl =
+    process.env.ELECTRON_START_URL ||
+    url.format({
+      pathname: path.join(__dirname, "./dist/index.html"),
+      protocol: "file:",
+      slashes: true,
+    });
+
+  win.loadURL(startUrl);
+
+  process.env.ELECTRON_START_URL ? win.webContents.openDevTools() : null;
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
